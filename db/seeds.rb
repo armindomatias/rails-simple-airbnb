990057fba228408b8a75892706ae372daf920577
Armindo Matias <armindomatiasg@gmail.com>
2023-08-16 17:04:35 +0100

    flats exercise without the query string search optional

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+10.times do
+  Flat.create!(
+    name: Faker::FunnyName.four_word_name,
+    address: Faker::Address.full_address,
+    description:  Faker::Lorem.paragraph(sentence_count: 5),
+    picture: 'https://images.unsplash.com/photo-1516501312919-d0cb0b7b60b8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2204&q=80',
+    price_per_night: rand(25...1000),
+    number_of_guests: (1..12).to_a.sample
+  )
+end
